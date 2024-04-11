0a2bd6c96c18dde737068c85ac83863b70d6f929
rpsimonds <rpsimonds@beaglebay.com>
2024-04-11 16:45:20 -0700

    Add LED Control & Change AT Command Port

--- main/app_main.c
+++ main/app_main.c
@@ -12,11 +12,29 @@
 
 void app_main(void)
 {
+
+    //zero-initialize the config structure.
+    gpio_config_t io_conf = {};
+    //disable interrupt
+    io_conf.intr_type = GPIO_INTR_DISABLE;
+    //set as output mode
+    io_conf.mode = GPIO_MODE_OUTPUT;
+    //bit mask of the pins that you want to set,e.g.GPIO18/19
+    io_conf.pin_bit_mask = GPIO_OUTPUT_PIN_SEL;
+    //disable pull-down mode
+    io_conf.pull_down_en = 0;
+    //disable pull-up mode
+    io_conf.pull_up_en = 0;
+    //configure GPIO with the given settings
+    gpio_config(&io_conf);
+    gpio_set_level(GPIO_OUTPUT_IO_0, 0);
+    gpio_set_level(GPIO_OUTPUT_IO_1, 0);
     esp_at_main_preprocess();
 
     ESP_ERROR_CHECK(nvs_flash_init());
     ESP_ERROR_CHECK(esp_at_netif_init());
     ESP_ERROR_CHECK(esp_event_loop_create_default());
-
+    gpio_set_level(GPIO_OUTPUT_IO_0, 1);
     esp_at_init();
+    gpio_set_level(GPIO_OUTPUT_IO_1, 1);
 }
